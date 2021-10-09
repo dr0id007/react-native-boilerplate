d219002300d498039c3e3748ef14e42e8ca7847a
dr0id007 <devarora745@gmail.com>
2021-10-09 11:24:39 +0530

    remove google service

--- app/routes/appRoutes.tsx
+++ app/routes/appRoutes.tsx
@@ -4,32 +4,10 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Home } from '../screens'
 import { APP_ROUTES } from './navigationConstant'
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList,
-} from '@react-navigation/drawer'
 
 const { height } = Dimensions.get('window')
 
 const Stack = createStackNavigator()
-const Drawer = createDrawerNavigator()
-
-function CustomDrawerContent(props: any) {
-  return (
-    <DrawerContentScrollView
-      {...props}
-      contentContainerStyle={{ justifyContent: 'space-around', flex: 1 }}>
-      <View>
-        <DrawerItemList {...props} />
-      </View>
-      <View style={{ display: 'flex', justifyContent: 'flex-start' }}>
-        <Text style={styles.footerText}>{'Shinobi Forest'}</Text>
-        <Text style={styles.footerText}>{'1.1.0'}</Text>
-      </View>
-    </DrawerContentScrollView>
-  )
-}
 
 const styles = StyleSheet.create({
   footerText: {
@@ -43,21 +21,9 @@ const styles = StyleSheet.create({
 const AppRoutes = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        drawerStyle={{
-          backgroundColor: '#50A387',
-          width: 190,
-        }}
-        drawerContentOptions={{
-          activeTintColor: 'white',
-          activeBackgroundColor: '#89C0AD',
-          inactiveTintColor: 'white',
-          itemStyle: {},
-        }}
-        initialRouteName="Home"
-        drawerContent={props => <CustomDrawerContent {...props} />}>
-        <Drawer.Screen name={APP_ROUTES.Home} component={Home} />
-      </Drawer.Navigator>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name={APP_ROUTES.Home} component={Home} />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
